19cb367bf08d70aad13d1df4ab2b2b9bf05de35d
Lisa LaRochelle <larochelle.lisa@gmail.com>
2019-07-17 03:14:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,7 @@ class Dog
   
   def initialize(name, breed = "Mutt")
     @name = name
+    @breed = breed
   end
   
-  def name= (name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
 end 
